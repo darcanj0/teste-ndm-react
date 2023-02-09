ee5c50705ed6bbc88b155ef78ec1c0c872ca8f3e
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-02-09 19:51:13 -0300

    feat(player): create form for player creation

--- src/components/players/index.tsx
+++ src/components/players/index.tsx
@@ -1,19 +1,89 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const createPlayerSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required("Name is required")
+    .min(3, "Name is too short")
+    .max(50, "Name is too long"),
+  age: yup
+    .number()
+    .required("Age is required")
+    .max(100, "Age must be between 18 and 100")
+    .min(18, "Age must be between 18 and 100"),
+});
+
+interface IPlayerCreationData {
+  name: string;
+  age: number;
+}
 
 export const PlayersSection = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IPlayerCreationData>({
+    resolver: yupResolver(createPlayerSchema),
+  });
+
+  const submitForm = (data: IPlayerCreationData) => {
+    console.log(data);
+  };
+
   return (
     <Box
+      component={"form"}
+      onSubmit={handleSubmit(submitForm)}
       sx={{
         background: "#dddfee",
-        boxShadow:
-          "20px -20px 60px #bcbeca, -20px 20px 60px #feffff",
+        boxShadow: "20px -20px 60px #bcbeca, -20px 20px 60px #feffff",
         border: "1px solid rgba( 255, 255, 255, 0.18 )",
         backdropFilter: "blur( 10.5px )",
         WebkitBackdropFilter: "blur( 10.5px )",
         borderRadius: "10px",
         width: "25rem",
         height: "30rem",
+        display: "flex",
+        flexDirection: "column",
+        padding: "10px",
+        justifyContent: "space-evenly",
+        alignItems: "center",
       }}
-    ></Box>
+    >
+      <TextField
+        {...register("name")}
+        id="outlined-basic"
+        label="Name"
+        variant="outlined"
+        fullWidth
+        error={!!errors.name}
+        helperText={errors.name?.message}
+      />
+
+      <TextField
+        id="outlined-basic"
+        label="Age"
+        variant="outlined"
+        type={"number"}
+        fullWidth
+        helperText={errors.age?.message}
+        {...register("age")}
+        error={!!errors.age?.message}
+      />
+
+      <Button
+        variant="contained"
+        type="submit"
+        sx={{
+          width: "50%",
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
   );
 };
