e02e45f5668b8000c47619cf715732c235be7672
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2023-02-09 18:20:37 -0300

    fix: correct navbar logic

--- src/components/players/index.tsx
+++ src/components/players/index.tsx
@@ -4,9 +4,15 @@ export const PlayersSection = () => {
   return (
     <Box
       sx={{
-        background: "red",
-        width: "50px",
-        height: "50px",
+        background: "#dddfee",
+        boxShadow:
+          "20px -20px 60px #bcbeca, -20px 20px 60px #feffff",
+        border: "1px solid rgba( 255, 255, 255, 0.18 )",
+        backdropFilter: "blur( 10.5px )",
+        WebkitBackdropFilter: "blur( 10.5px )",
+        borderRadius: "10px",
+        width: "25rem",
+        height: "30rem",
       }}
     ></Box>
   );
